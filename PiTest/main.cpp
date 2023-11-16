b9c886bf6092487cd28c91613e566bc650695a8e
Quant1co <115133527+Quant1co@users.noreply.github.com>
2023-11-16 21:11:05 +0300

    Добавьте файлы проекта.

---
+++ PiTest/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+const int N = 10;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int mass[N], max, min;
+
+	cout << "Ёлементы: |";
+	for (int r = 0; r < N; r++)
+	{
+		mass[r] = rand() % 99;
+		cout << mass[r] << "|";
+	}
+	cout << endl;
+
+	max = mass[0];
+	min = mass[0];
+	for (int r = 1; r < N; r++)
+	{
+		if (max < mass[r]) max = mass[r];
+		if (min > mass[r]) min = mass[r];
+	}
+	cout << "Min: " << min << endl;
+	cout << "Max: " << max << endl;
+
+	return 0;
+}
